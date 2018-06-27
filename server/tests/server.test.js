d96e71ef49e629dedf0d776a1fcb2e0c39d277ff
Colin MJ <colinmj@telus.net>
2018-06-26 19:54:29 -0700

    Added get tests for todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,23 @@ const expect = require('expect');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [
+  {
+    text: 'first text todo'
+  },
+  {
+    text: 'second text todo'
+  }
+];
+
 beforeEach(done => {
-  Todo.remove({}).then(() => {
-    done();
-  });
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => {
+      done();
+    });
 });
 
 describe('POST /todos', () => {
@@ -27,7 +40,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-        Todo.find()
+        Todo.find({ text })
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -49,12 +62,24 @@ describe('POST /todos', () => {
         }
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(err => {
             done(err);
           });
       });
+  });
+});
+
+describe('GET/Todos', () => {
+  it('should get all todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });

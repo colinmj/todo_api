4cf1e15260847d8aa058a64e561ca66cc520852e
Colin MJ <colinmj@telus.net>
2018-06-27 17:27:36 -0700

    Added jwt tokens

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//todos
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -104,6 +105,24 @@ app.patch('/todos/:id', (req, res) => {
     })
     .catch(err => {
       res.status(400).send();
+    });
+});
+
+//users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken().then(token => {
+        res.header('x-auth', token).send(user);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).send(err);
     });
 });
 
